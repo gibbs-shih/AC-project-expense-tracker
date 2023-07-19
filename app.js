5bce89745d464711668df7dc130ac3912f64aa1c
gibbs-shih <shih.gibbs@gmail.com>
2023-07-20 01:32:28 +0800

    init express environment

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const exphbs = require('express-handlebars')
+const port = 3000
+const db = require('./config/mongoose')
+
+const app = express()
+app.engine("hbs", exphbs({ defaultLayout: "main", extname: ".hbs"}))
+app.set("view engine", "hbs")
+
+app.listen(port, () => {
+  console.log(`Express is listening on http://localhost:${port}`)
+})
