5a504d8f5a0329af2f674a6f993ba9ca51247cad
gibbs-shih <shih.gibbs@gmail.com>
2023-07-20 02:39:01 +0800

    init routes

--- app.js
+++ app.js
@@ -2,10 +2,16 @@ const express = require('express')
 const exphbs = require('express-handlebars')
 const port = 3000
 const db = require('./config/mongoose')
+const routes = require('./routes/index')
+const methodOverride = require('method-override')
 
 const app = express()
 app.engine("hbs", exphbs({ defaultLayout: "main", extname: ".hbs"}))
 app.set("view engine", "hbs")
+
+app.use(express.urlencoded({extended: true}))
+app.use(methodOverride("_method"))
+app.ues(routes)
 
 app.listen(port, () => {
   console.log(`Express is listening on http://localhost:${port}`)
---
+++ routes/modules/expenses.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const router = express.Router
+
+// create
+router.post('/', (req, res) => {
+
+})
+
+// edit
+router.get('/:id/edit', (req, res) => {
+
+})
+
+router.put('/:id', (req, res) => {
+
+})
+
+// delete
+router.delete('/:id', (req, res) => {
+
+})
+
+module.exports = router
