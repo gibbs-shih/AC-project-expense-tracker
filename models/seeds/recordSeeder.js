6e406475442ac155d0f7c1c90040a2f686e348d3
gibbs-shih <shih.gibbs@gmail.com>
2023-07-21 01:44:45 +0800

    update recordSeeder with users and records

--- models/seeds/recordSeeder.js
+++ models/seeds/recordSeeder.js
@@ -8,11 +8,27 @@ const Category = require('../category.js')
 const User = require('../user.js')
 const Record = require("../record")
 
-const SEED_USERS = [
-  {
-    name: '廣治',
-  },
-  {
-    name: '小新',
+const seedUserList = require('../seeds/seedData/users.json')
+const seedRecordList = require('../seeds/seedData/records.json')
+
+db.once("open", async () => {
+  try {
+    console.log('Generating User and Record Seeders...')
+    for (let i = 0; i < seedUserList.length; i++) {
+      const user = await User.create(seedUserList[i])
+      const userId = user._id
+      for (let index of seedUserList[i].item) {
+        const record = seedRecordList[index]
+        record.userId = userId
+        const category = await Category.findOne({ name: record.category })
+        record.categoryId = category._id
+        await Record.create(record)
+      }
+    }
+    console.log('User and Record Seeders Generated!!!')
+    process.exit()
+  } catch (error) {
+    console.log(error)
+    process.exit(1)
   }
-]
+})
