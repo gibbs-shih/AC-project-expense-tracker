df6ac54059513e3535ba0aef2bbea4d403b8ba86
gibbs-shih <shih.gibbs@gmail.com>
2023-07-21 17:02:08 +0800

    update index.hbs and home.js

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -1,8 +1,22 @@
 const express = require('express')
 const router = express.Router()
 
-router.get('/', (req, res) => {
-  res.render('index')
-})
+const Category = require('../../models/category')
+const Record = require('../../models/record')
+
+router.get('/', async (req, res) => {
+  const categories = await Category.find().lean()
+  const records = await Record.find().lean().sort({ date: "asc" })
+  let totalAmount = 0
+  records.forEach(record => totalAmount += Number(record.amount))
+  const recordList = records.map(record => ({
+    ...record,
+    icon: categories.find(category => 
+      String(category._id) === String(record.categoryId)
+      )?.icon
+    }))
+  console.log(recordList)
+  res.render('index', { recordList, totalAmount })
+  })
 
 module.exports = router
