caa0763213a5b340314ea097d25887cb198a6cdd
gibbs-shih <shih.gibbs@gmail.com>
2023-07-23 11:07:08 +0800

    modify todoSeeder

--- models/seeds/recordSeeder.js
+++ models/seeds/recordSeeder.js
@@ -1,3 +1,5 @@
+const bcrypt = require('bcryptjs')
+
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config()
 }
@@ -15,11 +17,17 @@ db.once("open", async () => {
   try {
     console.log('Generating User and Record Seeders...')
     for (let i = 0; i < seedUserList.length; i++) {
-      const user = await User.create(seedUserList[i])
-      const userId = user._id
-      for (let index of seedUserList[i].item) {
+      const seedUser = seedUserList[i]
+      const salt = await bcrypt.genSalt(10)
+      const hash = await bcrypt.hash(seedUser.password, salt)
+      const user = await User.create({
+        name: seedUser.name,
+        email: seedUser.email,
+        password: hash
+      })
+      for (let index of seedUser.item) {
         const record = seedRecordList[index]
-        record.userId = userId
+        record.userId = user._id
         const category = await Category.findOne({ name: record.category })
         record.categoryId = category._id
         await Record.create(record)
