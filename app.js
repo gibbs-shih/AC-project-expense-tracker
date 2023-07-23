dc37b392b62228fd682771b0e9f147434118acdd
gibbs-shih <shih.gibbs@gmail.com>
2023-07-23 13:01:06 +0800

    chore: heroku init

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
-const port = 3000
+const PORT = process.env.PORT || 3000
 const db = require('./config/mongoose')
 const routes = require('./routes/index')
 const methodOverride = require('method-override')
@@ -47,6 +47,6 @@ app.use((req, res, next) => {
 })
 app.use(routes)
 
-app.listen(port, () => {
-  console.log(`Express is listening on http://localhost:${port}`)
+app.listen(PORT, () => {
+  console.log(`Express is listening on http://localhost:${PORT}`)
 })
